54c52483d1cbcf83a5cc04ae58af30f619af0b3c
Aleksandr Chudov <chudov.aleksandr94@gmail.com>
2017-09-20 16:21:11 +0300

    Added task1

---
+++ 1/Task1/Task1/Trie.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Trie
+    {
+        private Node Root { get; }
+
+        public Trie()
+        {
+            Root = new Node(null);
+        }
+
+        #region public
+
+        public bool Add(string element)
+        {
+            Node node = GetNode(element, true);
+            if (node.IsTerminal)
+            {
+                return false;
+            }
+            node.IsTerminal = true;
+            Update(node);
+            return true;
+        }
+
+        public bool Contains(string element)
+        {
+            return GetNode(element, false)?.IsTerminal ?? false;
+        }
+
+        public bool Remove(string element)
+        {
+            if (!Contains(element))
+            {
+                return false;
+            }
+            Remove(GetNode(element, false));
+            return true;
+        }
+
+        public int Size()
+        {
+            return Root.ElementsCount;
+        }
+
+        public int HowManyStartsWithPrefix(string prefix)
+        {
+            Node node = GetNode(prefix, false);
+            return node?.ElementsCount ?? 0;
+        }
+
+        #endregion
+
+        #region private
+
+        private Node GetNode(string element, bool createIfNotExist)
+        {
+            Node node = Root;
+            foreach (char c in element)
+            {
+                if (!node.Children.ContainsKey(c))
+                {
+                    if (!createIfNotExist)
+                    {
+                        return null;
+                    }
+                    node.Children.Add(c, new Node(node, c));
+                }
+                node = node.Children[c];
+            }
+            return node;
+        }
+
+        private void Update(Node node)
+        {
+            while (node != null)
+            {
+                node.Update();
+                node = node.Parent;
+            }
+        }
+
+        private void Remove(Node node)
+        {
+            if (node == null)
+            {
+                throw new ArgumentNullException();
+            }
+            node.IsTerminal = false;
+            Update(node);
+            while (!IsRoot(node) && node.IsLeaf && !node.IsTerminal)
+            {
+                var p = node.Parent;
+                p.Children.Remove(node.Symbol);
+                node = p;
+            }
+        }
+
+        private bool IsRoot(Node node)
+        {
+            return node.Parent == null;
+        }
+
+        #endregion
+
+        private class Node
+        {
+            public Dictionary<char, Node> Children { get; } = new Dictionary<char, Node>();
+            public bool IsTerminal { get; set; }
+
+            public char Symbol { get; }
+
+            public bool IsLeaf => Children.Count == 0;
+
+            public int ElementsCount { get; set; } = 0;
+
+            public Node Parent { get; }
+
+            public Node(Node parent, char c = '\0', bool isTerminal = false)
+            {
+                IsTerminal = isTerminal;
+                Symbol = c;
+                Parent = parent;
+            }
+
+            public void Update()
+            {
+                ElementsCount = Children.Sum(p => p.Value.ElementsCount) + Convert.ToInt32(IsTerminal);
+            }
+        }
+    }
+}
---
+++ 1/Task1/Task1Tests/TrieTests.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task1;
+
+namespace Task1Tests
+{
+    [TestClass()]
+    public class TrieTests
+    {
+        private Trie _trie;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _trie = new Trie();
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.IsTrue(_trie.Add("abacaba"));
+            Assert.IsTrue(_trie.Add("abcde"));
+            Assert.IsTrue(_trie.Add("qwerty"));
+
+            Assert.IsFalse(_trie.Add("abacaba"));
+            Assert.IsFalse(_trie.Add("abcde"));
+            Assert.IsFalse(_trie.Add("qwerty"));
+        }
+
+        [TestMethod()]
+        public void ContainsTest()
+        {
+            Assert.IsTrue(_trie.Add("abacaba"));
+            Assert.IsTrue(_trie.Add("abcde"));
+            Assert.IsTrue(_trie.Add("qwerty"));
+
+            Assert.IsTrue(_trie.Contains("qwerty"));
+            Assert.IsTrue(_trie.Contains("abcde"));
+            Assert.IsTrue(_trie.Contains("abacaba"));
+
+            Assert.IsFalse(_trie.Contains("12345"));
+            Assert.IsFalse(_trie.Contains("qwergh"));
+        }
+
+        [TestMethod()]
+        public void RemoveTest()
+        {
+            Assert.IsTrue(_trie.Add("abacaba"));
+            Assert.IsTrue(_trie.Add("abcde"));
+            Assert.IsTrue(_trie.Add("qwerty"));
+
+            Assert.IsTrue(_trie.Contains("abcde"));
+            Assert.IsTrue(_trie.Remove("abcde"));
+            Assert.IsFalse(_trie.Contains("abcde"));
+            Assert.IsFalse(_trie.Remove("abcde"));
+            Assert.IsFalse(_trie.Remove("qqqqqqq"));
+        }
+
+        [TestMethod()]
+        public void SizeTest()
+        {
+            Assert.AreEqual(_trie.Size(), 0);
+
+            Assert.IsTrue(_trie.Add("abacaba"));
+            Assert.AreEqual(_trie.Size(), 1);
+
+            Assert.IsTrue(_trie.Add("abcde"));
+            Assert.AreEqual(_trie.Size(), 2);
+
+            Assert.IsTrue(_trie.Add("qwerty"));
+            Assert.AreEqual(_trie.Size(), 3);
+
+            Assert.IsTrue(_trie.Remove("abacaba"));
+            Assert.AreEqual(_trie.Size(), 2);
+
+            Assert.IsFalse(_trie.Remove("qqqqqq"));
+            Assert.AreEqual(_trie.Size(), 2);
+
+            Assert.IsTrue(_trie.Remove("abcde"));
+            Assert.AreEqual(_trie.Size(), 1);
+
+            Assert.IsTrue(_trie.Remove("qwerty"));
+            Assert.AreEqual(_trie.Size(), 0);
+        }
+
+        [TestMethod()]
+        public void HowManyStartsWithPrefixTest()
+        {
+            Assert.IsTrue(_trie.Add("abacaba"));
+            Assert.IsTrue(_trie.Add("abcde"));
+            Assert.IsTrue(_trie.Add("qwerty"));
+
+            Assert.AreEqual(_trie.HowManyStartsWithPrefix("q"), 1);
+            Assert.AreEqual(_trie.HowManyStartsWithPrefix("a"), 2);
+            Assert.AreEqual(_trie.HowManyStartsWithPrefix("ab"), 2);
+            Assert.AreEqual(_trie.HowManyStartsWithPrefix("abac"), 1);
+            Assert.AreEqual(_trie.HowManyStartsWithPrefix("abde"), 0);
+
+            Assert.IsTrue(_trie.Add("a"));
+            Assert.IsTrue(_trie.Add("ab"));
+            Assert.IsTrue(_trie.Add("abc"));
+            Assert.IsTrue(_trie.Add("aba"));
+            Assert.IsTrue(_trie.Add("abac"));
+
+            Assert.AreEqual(_trie.HowManyStartsWithPrefix("a"), 7);
+            Assert.AreEqual(_trie.HowManyStartsWithPrefix("ab"), 6);
+            Assert.AreEqual(_trie.HowManyStartsWithPrefix("abc"), 2);
+            Assert.AreEqual(_trie.HowManyStartsWithPrefix("aba"), 3);
+        }
+    }
+}
